0e0f96e6f705b51cbab9e9f67420e12998e640d6
Dan Hogan <hogandanhogan@gmail.com>
2020-03-21 20:26:20 -0400

    Initial Commit

---
+++ #saunalyfe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  #saunalyfe
+//
+//  Created by Dan Hogan on 3/21/20.
+//  Copyright © 2020 Dan Hogan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
